923f6e3f8ce8b3e485095d41883f1ea3d7b8eeee
useridentify <102258970+useridentify@users.noreply.github.com>
2023-02-03 18:08:18 +0300

    tg.close()

--- main.py
+++ main.py
@@ -16,17 +16,12 @@ def start_fun(message):
    bot.send_message( message.chat.id, 'Привет, я бот Юздеска!)\nОткрой приложение по кнопке.', parse_mode="Markdown", reply_markup=webAppKeyboard()) #отправляем сообщение с нужной клавиатурой
 
 
-@bot.message_handler(content_types="text")
-def new_mes(message):
-   start_fun(message)
-
-
 @bot.message_handler(content_types="web_app_data") #получаем отправленные данные 
 def answer(webAppMes):
    print(webAppMes) #вся информация о сообщении
    print(webAppMes.web_app_data.data) #конкретно то что мы передали в бота
    bot.send_message(webAppMes.chat.id, f"Хочу добавить: {webAppMes.web_app_data.data}") 
-   #отправляем сообщение в ответ на отправку данных из веб-приложения 
+   #отправляем сообщение в ответ на отправку данных из веб-приложения
 
 if __name__ == '__main__':
    bot.infinity_polling()
