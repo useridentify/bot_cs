0ae20a74c499509942838c7680f2acc002cb8b30
useridentify <102258970+useridentify@users.noreply.github.com>
2023-02-04 21:11:54 +0300

    answerWebAppQuery

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import telebot
 
 bot = telebot.TeleBot('5958497453:AAGGF0d9Sl7S-SQbKnxFu6MomELQPcSH8wg')
 
-def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
-   keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
+def webAppKeyboardInline(): #создание inline-клавиатуры с webapp кнопкой
+   keyboard = types.InlineKeyboardMarkup(row_width=1) #создаем клавиатуру inline
    webApp = types.WebAppInfo("https://useridentify.github.io/bot_cs/") #создаем webappinfo - формат хранения url
-   one_butt = types.KeyboardButton(text="Открыть приложение", web_app=webApp) #создаем кнопку типа webapp
-   keyboard.add(one_butt) #добавляем кнопки в клавиатуру
+   one = types.InlineKeyboardButton(text="Веб приложение", web_app=webApp) #создаем кнопку типа webapp
+   keyboard.add(one) #добавляем кнопку в клавиатуру
 
    return keyboard #возвращаем клавиатуру
 
+
 @bot.message_handler(commands=['start']) #обрабатываем команду старт
 def start_fun(message):
-   bot.send_message( message.chat.id, 'Привет, я бот Юздеска!)\nОткрой приложение по кнопке.', parse_mode="Markdown", reply_markup=webAppKeyboard()) #отправляем сообщение с нужной клавиатурой
+   bot.send_message( message.chat.id, 'Привет, я бот Юздеска!)\nОткрой приложение по кнопке.', parse_mode="Markdown", reply_markup=webAppKeyboardInline()) #отправляем сообщение с нужной клавиатурой
 
 @bot.message_handler(content_types="text")
 def new_mes(message):
