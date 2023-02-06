ef006c62412b0a8b3ac2cc8f63397ef6148f9dd1
useridentify <102258970+useridentify@users.noreply.github.com>
2023-02-06 23:17:06 +0300

    ds

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def new_mes(message):
 def answer(webAppMes):
    print(webAppMes) #вся информация о сообщении
    print(webAppMes.web_app_data.data) #конкретно то что мы передали в бота
-   bot.send_message(webAppMes.chat.id, f"{webAppMes.web_app_data.data}") 
+   bot.send_message(webAppMes.chat.id, f"Ваш заказ.\n{webAppMes.web_app_data.data}") 
    #отправляем сообщение в ответ на отправку данных из веб-приложения 
 
 if __name__ == '__main__':
